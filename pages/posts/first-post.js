86e212ba93494dd53a7461b46c59b6a39be201f9
dlgurwls0529 <dlgurwls0529@gmail.com>
2022-05-13 10:28:37 +0900

    hello

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,3 +1,14 @@
+import Link from 'next/link';
+
 export default function FirstPost() {
-  return <h1>My name is Lee Hyeok Jin</h1>;
+  return (
+    <>
+      <h1>My name is Lee Hyeok Jin</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </>
+  );
 }
